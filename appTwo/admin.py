4f2a4a101ef9f0ed7615cb34420d2bd3559dd46a
larryvsdata <larryvsdata@gmail.com>
2019-03-23 19:56:53 -0700

    Added some migrations

--- appTwo/admin.py
+++ appTwo/admin.py
@@ -2,5 +2,8 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from appTwo.models import User
 
 # Register your models here.
+
+admin.site.register(User)
--- appTwo/models.py
+++ appTwo/models.py
@@ -4,3 +4,8 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    first_name = models.CharField(max_length = 128)
+    last_name = models.CharField(max_length = 128)
+    email = models.EmailField(max_length = 264 , unique = True)
